09ee8a2d27de105322e78955aca6981eacf3cdcd
Pedro Sales Muniz <46171624+salesp07@users.noreply.github.com>
2023-06-02 00:27:31 -0700

    improve card design

--- src/components/Projects/ProjectCard/ProjectCardElements.js
+++ src/components/Projects/ProjectCard/ProjectCardElements.js
@@ -6,6 +6,9 @@ export const Card = styled.div`
   margin-bottom: 4rem;
   grid-template-columns: 1fr;
   padding-bottom: 2rem;
+  overflow: hidden;
+  border-radius: 10px;
+  box-shadow: 0 5px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
     border-bottom: 0;
@@ -14,10 +17,8 @@ export const Card = styled.div`
 `;
 
 export const CardLeft = styled.div`
-  border-radius: 5px;
   justify-self: center;
   img {
-    border-radius: 5px;
     height: auto;
   }
 `;
