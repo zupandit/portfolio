e6f5db6997bb0d62cbb8aa6841793aa794f25305
Pedro Sales Muniz <46171624+salesp07@users.noreply.github.com>
2023-06-04 12:40:48 -0700

    potential fix for snake svg stuck in frame 1

--- src/components/Contributions/Contributions.js
+++ src/components/Contributions/Contributions.js
@@ -6,7 +6,7 @@ function Contributions() {
     <div className='Container' style={{marginTop: '5rem'}}>
         <div className="SectionTitle" style={{display: 'flex', alignItems:"center", flexWrap: 'wrap'}}>My Contributions&nbsp;{<FaGithubSquare/>} </div>
         <div className="BigCard" style={{padding: '0 10px 10px 10px'}}>
-        <img alt="snake eating my contributions" src="https://raw.githubusercontent.com/salesp07/salesp07/output/github-contribution-grid-snake.svg" />
+        <img alt="snake eating my contributions" src="https://raw.githubusercontent.com/salesp07/salesp07/output/github-contribution-grid-snake.svg?v=1" />
         </div>
     </div>
   )
