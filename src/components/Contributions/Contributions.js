0ac558376eb5d226dcc1c182644a67a34288511d
Pedro Sales Muniz <46171624+salesp07@users.noreply.github.com>
2023-06-04 02:48:34 -0700

    sdd contributiond gif to project

--- src/components/Contributions/Contributions.js
+++ src/components/Contributions/Contributions.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { FaGithubSquare } from 'react-icons/fa'
 
 function Contributions() {
   return (
     <div className='Container' style={{marginTop: '5rem'}}>
-        <div className="SectionTitle">Contributions</div>
-        <div className="BigCard">
-        <img alt="snake eating my contributions" src="https://github.com/salesp07/salesp07/blob/output/github-contribution-grid-snake.svg" />
+        <div className="SectionTitle" style={{display: 'flex', alignItems:"center", flexWrap: 'wrap'}}>My Contributions&nbsp;{<FaGithubSquare/>} </div>
+        <div className="BigCard" style={{padding: '0 10px 10px 10px'}}>
+        <img alt="snake eating my contributions" src="https://raw.githubusercontent.com/salesp07/salesp07/output/github-contribution-grid-snake.svg" />
         </div>
     </div>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,12 +6,14 @@ import Contact from "../components/Contact/Contact";
 import Footer from "../components/Footer/Footer";
 import FixSocialIcon from "../components/SocialIcon/FixSocialIcon";
 import ScrollToTop from "../components/SocialIcon/ScrollToTop";
+import Contributions from "../components/Contributions/Contributions";
 function Home() {
   return (
     <>
       <Hero />
       <Projects />
       <About />
+      <Contributions />
       <Contact />      
       <FixSocialIcon />
       <Footer />
